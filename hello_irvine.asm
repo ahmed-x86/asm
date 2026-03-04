e0a77b067cdad54c740e33a1ffccd2fd1cd6bc4c
ahmed-x86 <ahmdansmr7@gmail.com>
2026-03-04 12:04:10 +0200

    12:04PM

---
+++ hello_irvine.asm
@@ -0,0 +1,13 @@
+INCLUDE Irvine32.inc
+
+.data
+    msg BYTE "hi i use arch btw from win32 (Irvine Edition)", 0
+
+.code
+main PROC
+    mov edx, OFFSET msg    ; نضع مسار النص في مسجل edx
+    call WriteString       ; دالة جاهزة من مكتبة الدكتور للطباعة
+    call Crlf              ; دالة جاهزة للنزول سطر جديد
+    exit                   ; تعادل ExitProcess(0)
+main ENDP
+END main
