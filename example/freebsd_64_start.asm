1164cbaf8d7c845436494d9ce445edc0a9a41782
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-11 15:41:54 +0300

    freebsd_64_start.asm

---
+++ example/freebsd_64_start.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello from FreeBSD 64-bit on Arch Linux!", 10
+    len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov rax, 4
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
+    syscall
+
+    mov rax, 1
+    xor rdi, rdi
+    syscall
