fcdc874c308f0464003e7e5f76320d0678317adc
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-11 15:40:51 +0300

    freebsd_32_main.asm

---
+++ example/freebsd_32_main.asm
@@ -0,0 +1,20 @@
+section .data
+    msg db "hi i use arch btw by freebsd32 (main)", 10
+    len equ $ - msg
+
+section .text
+    global main
+
+main:
+    push dword len
+    push dword msg
+    push dword 1
+    push dword 0
+    mov eax, 4
+    int 0x80
+    add esp, 16
+
+    push dword 0
+    push dword 0
+    mov eax, 1
+    int 0x80
