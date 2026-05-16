8557a4d90ac8852052d43c16c0340a70edd0eeb9
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-16 14:26:03 +0300

    .

--- install-linux.sh
+++ install-linux.sh
@@ -409,7 +409,7 @@ set -e
 
 if [[ "$download_examples" =~ ^[Yy]$ ]]; then
   example_urls=(
-    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/linux_risc-v32e_main.asm"
+    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/linux_risc-v64_main.asm"
   )
 
   for url in "${example_urls[@]}"; do
