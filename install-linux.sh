72ed9e67f6fe20b0fe2753044562d62251770734
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-16 14:23:27 +0300

    .

--- install-linux.sh
+++ install-linux.sh
@@ -409,7 +409,7 @@ set -e
 
 if [[ "$download_examples" =~ ^[Yy]$ ]]; then
   example_urls=(
-    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/freebsd_64_main.asm"
+    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/win64_std_start.asm"
   )
 
   for url in "${example_urls[@]}"; do
