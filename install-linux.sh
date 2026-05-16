3f6b796cb9feebba246c321bc33c19e22126fa36
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-16 14:34:31 +0300

    .

--- install-linux.sh
+++ install-linux.sh
@@ -432,6 +432,7 @@ if [[ "$download_examples" =~ ^[Yy]$ ]]; then
     "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/win32_std_main.asm"
     "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/linux_arm32_start.asm"
     "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/linux_arm32_main.asm"
+    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/win32_irvine_main.asm"
   )
 
   for url in "${example_urls[@]}"; do
