68ea2a1c35c32b82d56c6dd61c7fe9b12f99e20d
ahmed-x86 <ahmdansmr7@gmail.com>
2026-05-16 14:50:34 +0300

    .

--- install-windows.ps1
+++ install-windows.ps1
@@ -37,7 +37,8 @@ $exampleUrls = @(
     "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/win32_std_main.asm",
     "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/linux_arm32_start.asm",
     "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/linux_arm32_main.asm",
-    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/win32_irvine_main.asm"
+    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/win32_irvine_main.asm",
+    "https://raw.githubusercontent.com/ahmed-x86/asm/refs/heads/main/example/freebsd_32_main.asm"
 )
 $msysBinDir = "C:\msys64\usr\bin"
 $targetPath = Join-Path $msysBinDir "asm-run.exe"
